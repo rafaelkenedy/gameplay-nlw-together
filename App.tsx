ba1cae2dfc62b66b62fa253bd3f273f7b2c8b62a
rafaelkenedy <k.rafaelalves@gmail.com>
2021-06-25 02:17:51 -0300

    External Fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useFonts } from 'expo-font' 
+import { useFonts } from 'expo-font'  
 import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter'
 import { Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani' 
 import AppLoading from 'expo-app-loading'
